bcd72678bf6cf0a1167e8e49070bfe07922cbff7
ManuDeveloper <manuepeva@gmail.com>
2017-12-22 13:06:35 -0500

    routing asp.net custom accept header

--- WebApiV2/Custom/CustomControllerSelector.cs
+++ WebApiV2/Custom/CustomControllerSelector.cs
@@ -26,22 +26,22 @@ namespace WebApiV2.Custom
             var controllerName = routeData.Values["controller"].ToString();
 
             string versionNumber = "1";
-            var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            if (versionQueryString["v"] != null)
-            {
-                versionNumber = versionQueryString["v"];
-            }
+            //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
+            //if (versionQueryString["v"] != null)
+            //{
+            //    versionNumber = versionQueryString["v"];
+            //}
 
-            string customHeader = "X.-StudentService-Version";
-            if (request.Headers.Contains(customHeader))
-            {
-                versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            //string customHeader = "X.-StudentService-Version";
+            //if (request.Headers.Contains(customHeader))
+            //{
+            //    versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
 
-                if (versionNumber.Contains(","))
-                {
-                    versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
-                }
-            }
+            //    if (versionNumber.Contains(","))
+            //    {
+            //        versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
+            //    }
+            //}
 
             //var acceptHeader = request.Headers.Accept.
             //    Where(a => a.Parameters.Count(p => p.Name.ToLower() == "version") > 0);
@@ -50,16 +50,18 @@ namespace WebApiV2.Custom
             //    versionNumber = acceptHeader.First().Parameters.
             //        First(p => p.Name.ToLower() == "version").Value;
             //}
+            var regex = @"application\/mpv\.pragimtech\.([a-z]+)\.v(?<version>[0-9]+)\+([a-z]+)";
 
-            //var regex = @"application\/mpv\.pragimtech\.([a-z]+)\.v(?<version>[0-9]+)\+([a-z]+)";
+            var acceptHeader = request.Headers.Accept.
+                Where(a => Regex.IsMatch(a.MediaType, regex, RegexOptions.IgnoreCase));
+            if (acceptHeader.Any())
+            {
+                var match = Regex.Match(acceptHeader.First().MediaType, regex, RegexOptions.IgnoreCase);
+                versionNumber = match.Groups["version"].Value;
+            }
 
-            //var acceptHeader = request.Headers.Accept.
-            //    Where(a => Regex.IsMatch(a.MediaType, regex, RegexOptions.IgnoreCase));
-            //if (acceptHeader.Any())
-            //{
-            //    var match = Regex.Match(acceptHeader.First().MediaType, regex, RegexOptions.IgnoreCase);
-            //    versionNumber = match.Groups["version"].Value;
-            //}
+
+            HttpControllerDescriptor controllerDescriptor;
             if (versionNumber == "1")
             {
                 controllerName = controllerName + "V1";
@@ -69,10 +71,13 @@ namespace WebApiV2.Custom
                 controllerName = controllerName + "V2";
             }
 
+                 
 
-            if (controllers.TryGetValue(controllerName, out HttpControllerDescriptor controllerDescriptor))
+
+            if (controllers.TryGetValue(controllerName, out controllerDescriptor))
             {
                 return controllerDescriptor;
+              
             }
             return null;
         }
