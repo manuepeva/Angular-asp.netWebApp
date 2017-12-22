3a258a570fbda63fce298b41e726a864127491ae
ManuDeveloper <manuepeva@gmail.com>
2017-12-22 11:50:02 -0500

    custom accept header asp.net web api routing

---
+++ WebApiV2/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using WebApiV2.Custom;
+
+namespace WebApiV2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Services.Replace(typeof(IHttpControllerSelector), new CustomControllerSelector(config));
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("Accept: application/mpv.pragimtech.students.v1+json"));
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("Accept: application/mpv.pragimtech.students.v2+json"));
+
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("Accept: application/mpv.pragimtech.students.v1+xml"));
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("Accept: application/mpv.pragimtech.students.v2+xml"));
+            //config.Routes.MapHttpRoute(
+            //    name: "StudentV2",
+            //    routeTemplate: "api/v2/students/{id}",
+            //    defaults: new { id = RouteParameter.Optional, controller = "StudentV2" }
+            //);
+        }
+    }
+}
---
+++ WebApiV2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiV2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
